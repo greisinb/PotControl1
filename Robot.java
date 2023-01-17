5f20172ab91314b3a6d0a7b02b381b83e04ad435
greisinb <blake.greisinger@bremertonschools.org>
2023-01-17 08:45:44 -0800

    Add files via upload

---
+++ Robot.java
@@ -0,0 +1,71 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.Joystick;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private Joystick leftStick;
+  private CANSparkMax actuator;
+  //public AnalogPotentiometer pot;
+  public AnalogInput pistonSensor;
+
+  
+public void robotInit() {
+  actuator = new CANSparkMax(6, MotorType.kBrushless);
+  leftStick = new Joystick(1);
+  //pot = new AnalogPotentiometer(1,500,0);
+  pistonSensor = new AnalogInput(0);
+  }
+  @Override
+  public void robotPeriodic() {
+    
+SmartDashboard.putNumber("Raw Potentiometer Value", pistonSensor.getValue());
+  }
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {
+    actuator.set(-leftStick.getY());
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
+}
